5c66f33112643a3bce06606b99958f2aedbdc145
Linsey R.  & Nick Jarin <pair+linseyo.njarin@devbootcamp.com>
2017-04-06 15:58:18 -0700

    Add seed data for questions and implement individual question page

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,5 @@
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @user = User.find(@question.user_id)
+  erb :'questions/show'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 require 'faker'
 
+User.delete_all
+Question.delete_all
+
 20.times do
   User.create!( name: Faker::Name.name,
                 username: Faker::Internet.user_name,
                 email: Faker::Internet.email,
                 password: "password" )
+end
+
+20.times do
+  Question.create!( title: Faker::Hacker.ingverb,
+                    description: Faker::Hacker.say_something_smart,
+                    user_id: rand(1..20) )
 end
