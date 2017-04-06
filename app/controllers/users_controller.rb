f216f680660c237791bf6c5b8706f3746b179f54
Linsey R.  & Nick Jarin <pair+linseyo.njarin@devbootcamp.com>
2017-04-06 14:32:36 -0700

    Add POST route for creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
+get '/users' do
+  # This is the page that displays all users
+end
+
 get '/users/new' do
   erb :'users/new'
+end
+
+post '/users' do
+  User.create(params[:user])
+  redirect to '/users'
 end
