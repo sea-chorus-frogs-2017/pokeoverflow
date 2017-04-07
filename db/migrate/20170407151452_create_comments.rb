f5d323a53e0925c5c26f4a7f6b58d6d61cfef8e7
Julia(e) Xu & Linsey R <pair+jmxu94.linseyo@devbootcamp.com>
2017-04-07 15:49:13 -0700

    fix migration typo

--- db/migrate/20170407151452_create_comments.rb
+++ db/migrate/20170407151452_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table :coments do |t|
+    create_table :comments do |t|
       t.string :content
       t.integer :commentable_id
       t.string :commentable_type
