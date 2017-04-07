6cda918a761c2add7b52ee3037de49c37344d6fb
Julia(e) Xu & Linsey R <pair+jmxu94.linseyo@devbootcamp.com>
2017-04-07 10:54:47 -0700

    Create new question form and ability to create question into database

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,9 +3,18 @@ get '/questions' do
   erb :'questions/index'
 end
 
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @user = User.find(@question.user_id)
   erb :'questions/show'
 end
 
+post '/questions' do
+  Question.create!(params['question'])
+  redirect to '/questions'
+end
