cc50b3e5267a56d1a44eac791a9241bb08ffc553
linseyo <reedlinsey@gmail.com>
2017-04-08 09:17:34 -0700

    Edit migration for votes

--- db/migrate/20170408085609_create_votes.rb
+++ db/migrate/20170408085609_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
         create_table :votes do |t|
-          t.string :content
+          t.integer :value
           t.integer :user_id
           t.integer :voteable_id
           t.string :voteable_type
