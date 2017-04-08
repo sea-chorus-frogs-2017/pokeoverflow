e138cb821c48647a3f6af56f5dcae800cbc33fa8
linseyo <reedlinsey@gmail.com>
2017-04-08 10:24:35 -0700

    rename questiontitle partial, add code to display question vote value

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,11 @@
+# NOT SURE IF WE'RE GONNA NEED THIS FILE.
+# Just putting PSEUDOCODE HERE.
+
+# Displaying Votes:
+  # Using the _questiontitle partial which I've gone ahead and renamed to _questioninfo
+  # Vote VALUE for the question can be placed next to the question
+  # (If you look at Stackoverflow it just shows the votes on the question + number of answers)
+
+# For jQUERY & AJAX
+  # Refer to Hacker news
+  # Consider using .one to only allow a single click action. 
