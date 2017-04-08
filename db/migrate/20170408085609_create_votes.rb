29f826a9dbacd14e4133c33ad0a5f1584ecd69a6
linseyo <reedlinsey@gmail.com>
2017-04-08 09:16:15 -0700

    Create votes migration & spec model files

--- db/migrate/20170408085609_create_votes.rb
+++ db/migrate/20170408085609_create_votes.rb
@@ -1,4 +1,15 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+        create_table :votes do |t|
+          t.string :content
+          t.integer :user_id
+          t.integer :voteable_id
+          t.string :voteable_type
+
+          t.timestamps null: false
+        end
+      end
+    end
+
   end
 end
