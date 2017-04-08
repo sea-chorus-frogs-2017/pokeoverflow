c15beb14a437b47d213294348933a62bdfcf1d75
linseyo <reedlinsey@gmail.com>
2017-04-08 11:25:39 -0700

    Add Submit type to all voting buttons, set up simple POST route for buttons with hacker news logic of creating value and destroying value

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,3 @@
-# NOT SURE IF WE'RE GONNA NEED THIS FILE.
-# Just putting PSEUDOCODE HERE.
-
 # Displaying Votes:
   # Using the _questiontitle partial which I've gone ahead and renamed to _questioninfo
   # Vote VALUE for the question can be placed next to the question
@@ -8,4 +5,16 @@
 
 # For jQUERY & AJAX
   # Refer to Hacker news
-  # Consider using .one to only allow a single click action. 
+  # Consider using .one to only allow a single click action.
+
+# Following Hacker News Convention of voting
+post '/questions/:voteable_id/vote' do
+  question = Question.find(params[:voteable_id])
+  question.votes.create(value:1)
+end
+
+# As far as down-voting I'm thinking we'll have to either remove or destroy a vote value?
+post '/questions/:voteable_id/vote' do
+  question = Question.find(params[:voteable_id])
+  question.votes.destroy(value:1)
+end
