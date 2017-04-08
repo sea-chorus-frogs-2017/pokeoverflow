ee33d246b6ff0f8404821fff45f56fd165c73f3c
linseyo <reedlinsey@gmail.com>
2017-04-08 12:37:00 -0700

    remove comment on answers seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,21 @@ require 'faker'
 User.delete_all
 Question.delete_all
 
-50.times do
+100.times do
   User.create!( name: Faker::Name.name,
                 username: Faker::Internet.user_name,
                 email: Faker::Internet.email,
                 password: "password" )
 end
 
-50.times do
+100.times do
   Question.create!( title: Faker::Hipster.sentence,
                     description: Faker::Hipster.paragraph,
                     user_id: rand(1..50)
     )
 end
 
-50.times do
+100.times do
   Comment.create!(  content: Faker::Hipster.sentence,
                     user_id: rand(1..50),
                     commentable_id: rand(1..50),
