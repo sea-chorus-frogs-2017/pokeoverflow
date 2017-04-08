8a57c114bda00e3ffedee3d00405b2d5f49b627f
Julia(e) Xu & Linsey R <pair+jmxu94.linseyo@devbootcamp.com>
2017-04-07 18:36:16 -0700

    Fix merge conflict in create_comments migration

--- db/migrate/20170407151452_create_comments.rb
+++ db/migrate/20170407151452_create_comments.rb
@@ -2,10 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :content
-<<<<<<< HEAD
       t.integer :user_id
-=======
->>>>>>> master
       t.integer :commentable_id
       t.string :commentable_type
 
